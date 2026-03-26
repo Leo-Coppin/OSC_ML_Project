b0189aabb4b2eb1412d724c1eeaf77b7b86d2ad5
Leo-Coppin <lea.coppin@student.junia.com>
2026-03-26 09:52:58 +0100

    switch to run on ssh server

--- Compatibiliy_score_ANN_2.py
+++ Compatibiliy_score_ANN_2.py
@@ -16,54 +16,52 @@ y15 = pd.read_csv("Output_Compatibility_score15.csv", sep=';')
 
 selected_features15 = ["scaled_delta_lambda", "scaled_λ_A_absorption", "scaled_HOMO_D_LUMO_A", "scaled_HOMO_D", "scaled_HOMO_A", "scaled_delta_HOMO"]
 X15 = X15[selected_features15]
-i=1
-pairs = [[X10, y10], [X15, y15]]
-for pair in pairs:
-    X, y = pair
 
-    #spliting datas
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = X10, 
+y = y10
 
-    #conversion in numpy values
-    X_train = X_train.values
-    X_test  = X_test.values
-    y_train = y_train.values
-    y_test  = y_test.values
+#spliting datas
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    input_dim = X_train.shape[1]
+#conversion in numpy values
+X_train = X_train.values
+X_test  = X_test.values
+y_train = y_train.values
+y_test  = y_test.values
 
-    #Optuna Search
-    print("\nOptuna Search\n")
-    study = csf.run_optuna_search(X_train, y_train, input_dim, n_trials=100)
+input_dim = X_train.shape[1]
 
-    #Training final model with Optuna Search params
-    print("\nTraining final model\n")
-    final_model, final_hyperparams = csf.train_final_model(study.best_trial.params, X_train, y_train, input_dim, 500)
-    final_model.summary()
+#Optuna Search
+print("\nOptuna Search\n")
+study = csf.run_optuna_search(X_train, y_train, input_dim, n_trials=100)
 
-    #Evaluation on Final Test set
-    #print("\nModel Evaluation\n")
-    results, y_pred, y_true = csf.evaluate_model(final_model, X_test, y_test,target_cols)
-    results_df = pd.DataFrame(results).T
-    print(results_df.round(4))
+#Training final model with Optuna Search params
+print("\nTraining final model\n")
+final_model, final_hyperparams = csf.train_final_model(study.best_trial.params, X_train, y_train, input_dim, 500)
+final_model.summary()
 
-    with open(f"model{i}_parameters.txt", "w") as f:
-        for key, value in study.best_trial.params.items():
-            f.write(f"{key}: {value}\n")
-    print("\nModel saved")
-    i+=1
-    
-    
-    # one of best models 
-    """best_params = {
-        'embedding_dim' : 64,
-        'n_layers'      : 3,
-        'activation'    : 'relu',
-        'lr'            : 0.0013622778618182295,
-        'optimizer'     : 'adam',
-        'weight_decay'  : 0.000572749459793566,
-        'dropout_rate'  : 0.06628799569948823,
-        'use_batch_norm': True,
-        'batch_size'    : 128,
-        'patience'      : 6
-    }"""
+#Evaluation on Final Test set
+#print("\nModel Evaluation\n")
+results, y_pred, y_true = csf.evaluate_model(final_model, X_test, y_test,target_cols)
+results_df = pd.DataFrame(results).T
+print(results_df.round(4))
+
+with open(f"model10_parameters.txt", "w") as f:
+    for key, value in study.best_trial.params.items():
+        f.write(f"{key}: {value}\n")
+print("\nModel saved")
+
+
+# one of best models 
+"""best_params = {
+    'embedding_dim' : 64,
+    'n_layers'      : 3,
+    'activation'    : 'relu',
+    'lr'            : 0.0013622778618182295,
+    'optimizer'     : 'adam',
+    'weight_decay'  : 0.000572749459793566,
+    'dropout_rate'  : 0.06628799569948823,
+    'use_batch_norm': True,
+    'batch_size'    : 128,
+    'patience'      : 6
+}"""
--- GBM.py
+++ GBM.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import r2_score, mean_absolute_error
 
 X = pd.read_csv("Data_Compatibility_score15.csv", sep=';')
 y = pd.read_csv("Output_Compatibility_score15.csv", sep=';')
@@ -12,41 +15,28 @@ y = pd.read_csv("Output_Compatibility_score15.csv", sep=';')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 1. On entraîne le modèle MultiOutput avec les paramètres qui marchaient pour le PCE
-best_base = GradientBoostingRegressor(learning_rate=0.01, max_depth=4, n_estimators=100, subsample=0.8, random_state=42)
-multi_model = MultiOutputRegressor(best_base)
-multi_model.fit(X_train, y_train)
+base_gbm = GradientBoostingRegressor(learning_rate=0.01, max_depth=4, n_estimators=100, subsample=0.8, random_state=42)
+final_model = MultiOutputRegressor(base_gbm) 
 
-# 2. On fait la prédiction sur le test
-y_pred = multi_model.predict(X_test)
-
-<<<<<<< Updated upstream
-# 3. On calcule le R² pour CHAQUE sortie séparément
-# 'raw_values' permet d'avoir un score par colonne au lieu de la moyenne
-individual_r2 = r2_score(y_test, y_pred, multioutput='raw_values')
-
-# Affichage propre
-for name, score in zip(y.columns, individual_r2):
-    print(f"Score R² pour {name} : {score:.3f}")
-=======
 # I used grid search to find the best hyperparameters for the Gradient Boosting Regressor, but it is commented out to save time during execution. The best parameters found are used to initialize the base_gbm model.
-param_grid = {
-    'estimator__n_estimators': [100, 200, 300],
-    'estimator__learning_rate': [0.01, 0.05, 0.1],
-    'estimator__max_depth': [3, 4, 5],
-    'estimator__subsample': [0.8, 1.0]
-}
+# param_grid = {
+#     'estimator__n_estimators': [100, 200, 300],
+#     'estimator__learning_rate': [0.01, 0.05, 0.1],
+#     'estimator__max_depth': [3, 4, 5],
+#     'estimator__subsample': [0.8, 1.0]
+# }
 
-grid_search = GridSearchCV(
-    final_model, 
-    param_grid, 
-    cv=5, # 5-fold cross-validation
-    scoring='r2', # R² score as the evaluation metric, R² for FF is only around 0.29 wereas for the other three it is around 0.5, so it is harder to determine what impact the most the FF factor.
-    n_jobs=-1 # Use all available CPU cores for parallel processing
-)
+# grid_search = GridSearchCV(
+#     multi_model, 
+#     param_grid, 
+#     cv=5, # 5-fold cross-validation
+#     scoring='r2', # R² score as the evaluation metric, R² for FF is only around 0.29 wereas for the other three it is around 0.5, so it is harder to determine what impact the most the FF factor.
+#     n_jobs=-1 # Use all available CPU cores for parallel processing
+# )
 
-grid_search.fit(X_train, y_train)
+# grid_search.fit(X_train, y_train)
 
-final_model = grid_search.best_estimator_
+# final_model = grid_search.best_estimator_
 
 # print(f"Meilleurs paramètres : {grid_search.best_params_}")
 # print(f"Meilleur R² moyen : {grid_search.best_score_:.3f}")
@@ -81,28 +71,18 @@ fig.suptitle("Experimental vs. Predicted Values (Organic Solar Cells)", fontsize
 
 for i, col in enumerate(target_names):
     sns.scatterplot(x=df_test_real[col], y=df_pred_real[col], ax=axes[i], alpha=0.5, color='#2c7fb8', s=40)
->>>>>>> Stashed changes
     
-# 4. Feature importances par target
-feature_names = X.columns.tolist()
-target_names = y.columns.tolist()
+    axis_min = min(df_test_real[col].min(), df_pred_real[col].min())
+    axis_max = max(df_test_real[col].max(), df_pred_real[col].max())
+    axes[i].plot([axis_min, axis_max], [axis_min, axis_max], 'r--', lw=2)
+    
+    axes[i].set_title(f"Parity Plot: {col}", fontsize=15, fontweight='bold')
+    axes[i].set_xlabel("Experimental Value", fontsize=12)
+    axes[i].set_ylabel("Predicted Value", fontsize=12)
 
-importances_per_target = pd.DataFrame(
-    [est.feature_importances_ for est in multi_model.estimators_],
-    index=target_names,
-    columns=feature_names
-)
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.subplots_adjust(wspace=0.3, hspace=0.4)
 
-<<<<<<< Updated upstream
-print("\nImportance des features par target :")
-print(importances_per_target.round(3))
-
-# 5. Importance moyenne sur les 4 targets
-mean_importance = importances_per_target.mean(axis=0).sort_values(ascending=False)
-
-print("\nImportance moyenne (triée) :")
-print(mean_importance.round(3))
-=======
 plt.show()
 """
 # Récupération des importances par target
@@ -123,4 +103,3 @@ importances_per_target = importances_per_target.sort_values(by="Mean", ascending
 importances_per_target.to_csv("importance_target15.csv", index=True, sep=';')
 
 print(importances_per_target.round(3))
->>>>>>> Stashed changes
