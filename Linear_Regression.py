dca56478e2851d3da4c8bb44c449fd98b8defffe
Leo-Coppin <leo.coppin@student.junia.com>
2026-03-30 12:31:41 +0000

    Linear regression ready to run as soon as train/test split ready

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -38,10 +38,10 @@ y_PubChem = pd.read_csv("Output_Pubchem.csv", sep=';')
 
 Xy_pairs = [[X_RDKit, y_RDKit, "rdkit"], [X_Mordred, y_Mordred, "mordred"], [X_Morgan, y_Morgan, "morgan"], [X_MACCS, y_MACCS, "MACCS"], [X_PubChem, y_PubChem, "Pubchem"]]
 
-outputs = ["scaled_Voc", "scaled_Jsc", "scaled_FF", "scaled_PCE", "scaled_delta_HOMO", "scaled_delta_LUMO"]
+#outputs = ["scaled_Voc", "scaled_Jsc", "scaled_FF", "scaled_PCE", "scaled_delta_HOMO", "scaled_delta_LUMO"]
 base_lr = GradientBoostingRegressor()
 multi_output_model = MultiOutputRegressor(base_lr) 
-
+"""
 for pairs in Xy_pairs : 
     X, y, name = pairs
     y = y[outputs]
@@ -64,3 +64,58 @@ for pairs in Xy_pairs :
     
     print(f"Results for {name} :")
     print(results_df)
+"""
+
+outputs_CV = ["scaled_Voc", "scaled_Jsc", "scaled_FF", "scaled_PCE"]
+outputs_delta = ["scaled_delta_HOMO", "scaled_delta_LUMO"]
+X = X_Mordred
+y = y_Mordred
+
+#temporary -> waiting for leakless train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+y_CV_train = y_train[outputs_CV]
+y_CV_test = y_test[outputs_CV]
+y_delta_train = y_train[outputs_delta]
+y_delta_test = y_test[outputs_delta]
+
+
+print(y_CV_test)
+print(y_delta_test)
+
+model_CV = MultiOutputRegressor(base_lr) 
+model_delta = MultiOutputRegressor(base_lr)
+
+model_CV.fit(X_train, y_CV_train)
+model_delta.fit(X_train, y_delta_train)
+
+y_CV_pred = model_CV.predict(X_test)
+y_delta_pred = model_delta.predict(X_test)
+
+print(y_CV_pred)
+print(y_delta_pred)
+
+r2_CV = r2_score(y_CV_test, y_CV_pred, multioutput='raw_values')
+mae_CV = mean_absolute_error(y_CV_test, y_CV_pred, multioutput='raw_values')
+rmse_CV = np.sqrt(mean_squared_error(y_CV_test, y_CV_pred, multioutput='raw_values'))
+
+r2_delta = r2_score(y_delta_test, y_delta_pred, multioutput='raw_values')
+mae_delta = mean_absolute_error(y_delta_test, y_delta_pred, multioutput='raw_values')
+rmse_delta = np.sqrt(mean_squared_error(y_delta_test, y_delta_pred, multioutput='raw_values'))
+
+results_CV = pd.DataFrame({
+    'R2'  : r2_CV,
+    'MAE' : mae_CV,
+    'RMSE': rmse_CV
+}, index=outputs_CV)
+
+results_delta = pd.DataFrame({
+    'R2'  : r2_delta,
+    'MAE' : mae_delta,
+    'RMSE': rmse_delta
+}, index=outputs_delta)
+
+print("Results for Voc, Jsc FF and PCE :")
+print(results_CV)
+print("\nResults for delta_HOMO and delta_LUMO :")
+print(results_delta)
