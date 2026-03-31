e0260b9fa518f0e65497af7b10977cb209796c48
Leo-Coppin <leo.coppin@student.junia.com>
2026-03-31 20:46:12 +0000

    new datasets to run the GNN + correction of the Linear Regression Model

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
@@ -38,8 +37,9 @@ y_PubChem = pd.read_csv("Output_Pubchem.csv", sep=';')
 
 Xy_pairs = [[X_RDKit, y_RDKit, "rdkit"], [X_Mordred, y_Mordred, "mordred"], [X_Morgan, y_Morgan, "morgan"], [X_MACCS, y_MACCS, "MACCS"], [X_PubChem, y_PubChem, "Pubchem"]]
 
-#outputs = ["scaled_Voc", "scaled_Jsc", "scaled_FF", "scaled_PCE", "scaled_delta_HOMO", "scaled_delta_LUMO"]
-base_lr = GradientBoostingRegressor()
+outputs = ["scaled_Voc", "scaled_Jsc", "scaled_FF", "scaled_PCE", "scaled_delta_HOMO", "scaled_delta_LUMO"]
+
+base_lr = LinearRegression()
 multi_output_model = MultiOutputRegressor(base_lr) 
 """
 for pairs in Xy_pairs : 
@@ -63,59 +63,34 @@ for pairs in Xy_pairs :
     }, index=outputs)
     
     print(f"Results for {name} :")
-    print(results_df)
+    print(results_df.round(4))
 """
 
-outputs_CV = ["scaled_Voc", "scaled_Jsc", "scaled_FF", "scaled_PCE"]
-outputs_delta = ["scaled_delta_HOMO", "scaled_delta_LUMO"]
-X = X_Mordred
-y = y_Mordred
+X = X_Morgan
+y = y_Morgan[outputs]
 
 #temporary -> waiting for leakless train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-y_CV_train = y_train[outputs_CV]
-y_CV_test = y_test[outputs_CV]
-y_delta_train = y_train[outputs_delta]
-y_delta_test = y_test[outputs_delta]
 
 
-print(y_CV_test)
-print(y_delta_test)
+model = MultiOutputRegressor(base_lr) 
 
-model_CV = MultiOutputRegressor(base_lr) 
-model_delta = MultiOutputRegressor(base_lr)
+model.fit(X_train, y_train)
 
-model_CV.fit(X_train, y_CV_train)
-model_delta.fit(X_train, y_delta_train)
+y_pred = model.predict(X_test)
 
-y_CV_pred = model_CV.predict(X_test)
-y_delta_pred = model_delta.predict(X_test)
+r2 = r2_score(y_test, y_pred, multioutput='raw_values')
+mae = mean_absolute_error(y_test, y_pred, multioutput='raw_values')
+rmse = np.sqrt(mean_squared_error(y_test, y_pred, multioutput='raw_values'))
 
-print(y_CV_pred)
-print(y_delta_pred)
+results = pd.DataFrame({
+    'R2'  : r2,
+    'MAE' : mae,
+    'RMSE': rmse
+}, index=outputs)
 
-r2_CV = r2_score(y_CV_test, y_CV_pred, multioutput='raw_values')
-mae_CV = mean_absolute_error(y_CV_test, y_CV_pred, multioutput='raw_values')
-rmse_CV = np.sqrt(mean_squared_error(y_CV_test, y_CV_pred, multioutput='raw_values'))
 
-r2_delta = r2_score(y_delta_test, y_delta_pred, multioutput='raw_values')
-mae_delta = mean_absolute_error(y_delta_test, y_delta_pred, multioutput='raw_values')
-rmse_delta = np.sqrt(mean_squared_error(y_delta_test, y_delta_pred, multioutput='raw_values'))
 
-results_CV = pd.DataFrame({
-    'R2'  : r2_CV,
-    'MAE' : mae_CV,
-    'RMSE': rmse_CV
-}, index=outputs_CV)
-
-results_delta = pd.DataFrame({
-    'R2'  : r2_delta,
-    'MAE' : mae_delta,
-    'RMSE': rmse_delta
-}, index=outputs_delta)
-
-print("Results for Voc, Jsc FF and PCE :")
-print(results_CV)
-print("\nResults for delta_HOMO and delta_LUMO :")
-print(results_delta)
+print("Results for Voc, Jsc FF, PCE, delta HOMO and delta LUMO:")
+print(results.round(4))
